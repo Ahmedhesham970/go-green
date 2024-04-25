17413cdcea88ca281fa700549694e0cf3c9582cb
Ahmed hesham <ugs.1441186@ci.suez.edu.eg>
2024-04-25 16:56:18 +0200

    handle post and auth management

--- index.js
+++ index.js
@@ -45,6 +45,10 @@ app.all("*", (req, res, next) => {
 });
 app.use(globalError);
 
+app.get('/',(req,res)=>{
+  res.status(200).json('welcome to the server GoGreen!!')
+})
+
 app.listen(process.env.PORT, () =>
   console.log(`Server is running on port ${process.env.PORT}`)
 );
